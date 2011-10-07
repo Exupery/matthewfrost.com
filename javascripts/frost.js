379b65e4dcc2d4cec33477ba189f31e6962deac9
padatika <padatika@yahoo.com>
2011-10-07 04:25:01 -0400

    black->blue

--- javascripts/frost.js
+++ javascripts/frost.js
@@ -18,18 +18,18 @@ $(function() {
 
 $(document).ready(function() {
 	$.gchart.setDefaults({
-		backgroundColor: '#000000',
-		titleColor: '#FFFFFF'
+		backgroundColor: '#e1e2ea',
+		titleColor: '#081530'
 	});
 	$( '#sourceChart' ).gchart({
 		type: 'pie',
 		title: 'Experience distribution',
 		pieOrientation: 108,
 		legend: 'right',
-		legendColor: '#FFFFFF',
+		legendColor: '#081530',
 		legendSize: 16,
 		extension: {chdl: 'work|school|personal'},
-		series: [$.gchart.series('Source', [33, 133, 34],['#3db918','#173db9','#b9173d'])]
+		series: [$.gchart.series('Source', [33, 133, 34],['#f5f50a','#00c400','#c40000'])]
 	});
 	$( '#expChart' ).gchart({
 		type: 'barHorizGrouped',
@@ -38,8 +38,8 @@ $(document).ready(function() {
 		axes: [$.gchart.axis(
 				'left', 
 				['jsp','perl','ror','c#','jquery','json','ajax','mysql','javascript','php','java']
-				,'#FFFFFF',16)],
-		series: [$.gchart.series('Amount',[100,85,95,90,85,75,80,15,25,20,30],'#c8eed0')]
+				,'#081530',16)],
+		series: [$.gchart.series('Amount',[100,85,95,90,85,75,80,15,25,20,30],'#8c9fdc')]
 	});
 });
 
@@ -49,9 +49,9 @@ function oom() {
 
 function updateChart(w, s, p, c) {
 	setTimeout(function(){$('#sourceChart').gchart('change', 'pieOrientation', s);}, 25);
-	$('#sourceChart').gchart('change', 'series', [$.gchart.series('Source', [w, s, p],['#3db918','#173db9','#b9173d'])]);
-	var color = ['#c8eed0','#c8eed0','#c8eed0','#c8eed0','#c8eed0','#c8eed0','#c8eed0','#c8eed0','#c8eed0','#c8eed0','#c8eed0'];
-	color[c] = '#faff61';
+	$('#sourceChart').gchart('change', 'series', [$.gchart.series('Source', [w, s, p],['#f5f50a','#00c400','#c40000'])]);
+	var color = ['#8c9fdc','#8c9fdc','#8c9fdc','#8c9fdc','#8c9fdc','#8c9fdc','#8c9fdc','#8c9fdc','#8c9fdc','#8c9fdc','#8c9fdc'];
+	color[c] = '#173db9';
 	$('#expChart').gchart('change', 'series', [$.gchart.series('Amount',[100,85,95,90,85,75,80,15,25,20,30],color)]);
 }
 
@@ -69,8 +69,8 @@ function setSliders(lang) {
 	if (lang == "perl") pieArray = [0,97,3,9];
 	if (lang == "jsp") pieArray = [0,25,75,10];
 	updateChart(pieArray[0], pieArray[1], pieArray[2], pieArray[3]);
-	$( ".langtech" ).css("color", "#c8eed0");
-	$( "#"+lang ).css("color", "#faff61");
+	$( ".langtech" ).css("color", "#8c9fdc");
+	$( "#"+lang ).css("color", "#173db9");
 	$( "#summarytxt" ).html(getSummary(lang));
 
 }
@@ -87,7 +87,7 @@ function getSummary(lang) {
 			"with my brokerage's API. This was eventually abandoned in favor of a Java app using the FIX protocol";
 	if (lang == "ror") html = "My first app put into production was written in RoR (later re-written in PHP).  I wasn't " +
 			"crazy about RoR, but I will say "+
-			"<a href='http://railsforzombies.org/' target='_blank'><span class='hotel'>Rails for Zombies</span></a>"+
+			"<a href='http://railsforzombies.org/' target='_blank'><span class='atext'>Rails for Zombies</span></a>"+
 			" is by far the most entertaining way to learn a language";
 	if (lang == "perl") html = "Thus far used almost entirely for school with only the occasional " +
 			"non-academic use";
@@ -96,7 +96,7 @@ function getSummary(lang) {
 	if (lang == "mysql") html = "Nearly every project I've worked on has involved utlizing a MySQL database";
 	if (lang == "ajax") html = "AJAX is used in nearly everything web related I code, " +
 			"it's a critical tool to go from web-page to web-app";
-	if (lang == "json") html = "Given the choice I use JSON whenever possibel, except in the rare cases where " +
+	if (lang == "json") html = "Given the choice I use JSON whenever possible, except in the rare cases where " +
 			"using XML would offer a significant benefit";
 	if (lang == "jquery") html = "A lot of my earlier JavaScript work was done without the aid of a framework.  Eventually I " +
 			"played around with both Dojo and jQuery before deciding jQuery was the better choice";
