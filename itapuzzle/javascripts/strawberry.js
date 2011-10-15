99cd5d252a0444e2473715a773c1b400d46d6557
padatika <padatika@yahoo.com>
2011-10-15 05:57:36 -0400

    ita

--- itapuzzle/javascripts/strawberry.js
+++ itapuzzle/javascripts/strawberry.js
@@ -16,48 +16,54 @@ function start() {
         mapFields();
 }
 
+function mapFields() {
+	var fld = new Field(testField);
+	findOptimal(fld);
+}
+
+function findOptimal(field) {
+	var greenhouses = new Array();
+	for (var i in field.edges) {
+		var topLeft = field.edges[i];
+		//console.log(field.edges[i].row+' '+field.edges[i].col);
+		for (var j=i; j < field.edges.length; j++) {
+			var bottomRight = field.edges[j];
+			var grnhs = new Greenhouse(topLeft, bottomRight);
+			console.log(grnhs.cost);
+		}
+	}
+	console.log(greenhouses.length);
+}
+
 function Field(fieldArray) {
 	this.numRows = fieldArray.length;
 	this.numCols = fieldArray[0].length;
 	this.edges = new Array();
-//	this.right = 0;
-//	this.left = this.numCols;
-//	this.lower = 0;
-//	this.upper = this.numRows;
-//	for (var i in fieldArray) {
-//		var s = fieldArray[i];
-//		if (s.indexOf('@') != -1) {
-//			if (i < this.upper) this.upper = i;
-//			if (i > this.lower) this.lower = i;
-//			if (i < this.left) this.left = s.indexOf('@');
-//		}
-//		if (i > this.right && s.lastIndexOf('@') != -1) this.right = s.lastIndexOf('@');
-//	}
+	
 	for (var i in fieldArray) {
 		var line = fieldArray[i];
-		var prevLine = (i > 0) ? fieldArray[i] : '.';
 		for (var j=0; j < line.length; j++) {
-			if (line.charAt(j) == '@' &&
-				(line.charAt(j-1) == '.' || line.charAt(j+1) == '.' || j==0 || j==this.numCols-1) &&
-				(prevLine.charAt(j) == '.' || fieldArray[i+1].charAt(j) == '.' || i==0 || i==this.numRows-1)
-				) {
-				console.log(j);
+			if (line.charAt(j) == '@' && (line.charAt(j-1) == '.' || line.charAt(j+1) == '.' || j==0 || j==this.numCols-1)) {
+				this.edges.push(new Edge(i, j));
 			}
 		}
 	}
 }
 
-function Edge(x, y) {
-	this.x = x;
-	this.y = y;
+function Edge(r, c) {
+	this.row = r;
+	this.col = c;
 }
 
-function Rectangle(ul, br) {
-	this.upperLeft = ul;
-	this.bottomRight = br;
+function Greenhouse(ul, br) {
+//	this.upperLeft = ul;
+//	this.upperRight = ur;
+//	this.bottomLeft = bl;
+//	this.bottomRight = br;
+//	var width = br.col - ul.col;
+//	var height = br.row - ul.row;
+	var area = (br.col - ul.col) * (br.row - ul.row);
+	this.cost = (area > 1) ? 10 + area : 11;
+
 }
 
-function mapFields() {
-    	var fld = new Field(testField);
-    	//console.log(fld.upper+' '+fld.lower+' '+fld.left+' '+fld.right);
-}
