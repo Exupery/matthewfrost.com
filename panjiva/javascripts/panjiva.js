1994880b57ebebf4afce9784100a0f438d3fe4e5
frost <mfrost@bostontechnologies.com>
2012-01-04 03:02:32 -0800

    panjiva

--- panjiva/javascripts/panjiva.js
+++ panjiva/javascripts/panjiva.js
@@ -14,7 +14,8 @@ $(function() {
 		var diff = Math.abs(distance - lastDistance) / Math.min(distance, lastDistance);
 		//console.log(distance+" "+diff);
 		if (distance < 25 || (distance < lastDistance && (diff > 0.25 || timeCheck > 200))) {
-			mouseMoved(event.pageX, event.pageY, distance);
+			var dir = (btnPos.left-event.pageX > 0) ? 1 : 0;
+			mouseMoved(1, distance);
 			lastMove = event.timeStamp;
 			//console.log(" move ");
 		}
@@ -22,28 +23,27 @@ $(function() {
 	});
 });
 	
-function mouseMoved(mouseX, mouseY, distance) {
+function mouseMoved(dir, distance) {
 	var btnPos = $('#btn').position();		
 	//var string = 'pageX: '+ mouseX +' pageY: '+ mouseY +' btnLeft: '+ btnPos.left +' btnTop: '+ btnPos.top;
 	//console.log(string);
 	var width = $(document).width();
 	var height = $(document).height();
-	var moveX = (distance < 150) ? (width / 10) : Math.random() * 50;
-	var moveY = (distance < 150) ? (height / 10) : Math.random() * 50;
-	var dir = 1;//Math.random(); //use same direction for X & Y to prevent excessive jumpiness
-	var string = mouseY+' '+moveY+' '+mouseX+' '+moveX+' '+width+' '+height;
-	console.log(string);
-	if (mouseY + moveY > height - 125 || mouseX + moveX > width - 125) {
-		console.log(">");
-		dir = 0;
-		console.log(dir);
-	} else if (mouseY - moveY < 50 || mouseX - moveX < 50) {
-		console.log("<");
-		dir = 1;
-		console.log(dir);
+	var moveX = (distance < 150) ? (width / 10) - distance: Math.random() * 50;
+	var moveY = (distance < 150) ? (height / 10) - distance: Math.random() * 50;
+	//var dir = 1;//Math.random(); //use same direction for X & Y to prevent excessive jumpiness
+	//var string = mouseY+' '+moveY+' '+mouseX+' '+moveX+' '+width+' '+height;
+	//console.log(string);
+	//var dir = 1;//Math.random();
+	var newX = (dir > 0.5) ? btnPos.left + moveX : btnPos.left - moveX;
+	var newY = (dir > 0.5) ? btnPos.top + moveY : btnPos.top - moveY;
+	if (newY > height - 50 || newX > width - 25) {
+		newX = moveX;
+		newY = moveY;
+	} else if (newY < 15 || newX < 15) {
+		newX = width - moveX;
+		newY = height - moveY;
 	}
-	var newX = (dir > 0.5) ? mouseX + moveX : mouseX - moveX;
-	var newY = (dir > 0.5) ? mouseY + moveY : mouseY - moveY;
 	$('#btn').css({left: newX, top: newY});
 }
 
