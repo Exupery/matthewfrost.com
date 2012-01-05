0392b08e6d0c6c73da12542076640111cb70b4ce
frost <mfrost@bostontechnologies.com>
2012-01-05 03:55:10 -0800

    panjiva

--- panjiva/javascripts/panjiva.js
+++ panjiva/javascripts/panjiva.js
@@ -27,16 +27,31 @@ $(function() {
 		var btnPos = $('#btn').position();
 		var xDist = btnPos.left - event.pageX;
 		var yDist = btnPos.top - event.pageY;
-		var moveX = 5;
-		var moveY = 5;
+		var moveX = 1;
+		var moveY = 1;
+		var timeCheck = event.timeStamp - lastMove;
+		//if (xDist > 0) console.log((xDist > 0));
 		var newX = (xDist > 0) ? btnPos.left + moveX : btnPos.left - moveX;
 		var newY = (yDist > 0) ? btnPos.top + moveY : btnPos.top - moveY;
-		if (Math.abs(xPrevDist - xDist) > 50 || Math.abs(yPrevDist - yDist) > 50) {
+		console.log(" xDist "+xDist+" xPrevDist "+xPrevDist+" yDist "+yDist+" yPrevDist "+yPrevDist);//+" "+btnPos.left+" "+btnPos.top);
+		if (((xDist > 0 && xPrevDist > xDist) && ((yDist >= 0 && yPrevDist >= yDist) || (yDist <= 0 && yPrevDist <= yDist)))
+			|| ((xDist < 0 && xPrevDist < xDist) && ((yDist >= 0 && yPrevDist >= yDist) || (yDist <= 0 && yPrevDist <= yDist))) 
+			|| ((yDist > 0 && yPrevDist > yDist) && (xDist >= 0 && xPrevDist >= xDist))
+			|| ((yDist < 0 && yPrevDist > yDist) && (xDist <= 0 && xPrevDist <= xDist))
+			) {
+//			xPrevDist = xDist;
+//			yPrevDist = yDist;
+			if (timeCheck > 50 ) {
+				lastMove = event.timeStamp;
+				//$('#btn').css({left: newX, top: newY});
+			}
+		}
+//		console.log(xDist+" "+yDist+" "+xPrevDist+" "+yPrevDist+" "+btnPos.left+" "+btnPos.top);
+		if (timeCheck > 25 ) {
 			xPrevDist = xDist;
 			yPrevDist = yDist;
-			$('#btn').css({left: newX, top: newY});
 		}
-		console.log(xDist+" "+yDist+" "+newX+" "+newY+" "+btnPos.left+" "+btnPos.top);
+		
 	});
 });
 	
