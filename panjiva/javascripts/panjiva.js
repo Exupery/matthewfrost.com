0f9cea4b55508e3a167c749c42e98c22c7dac08e
frost <mfrost@bostontechnologies.com>
2012-01-05 22:04:26 -0800

    panjiva

--- panjiva/javascripts/panjiva.js
+++ panjiva/javascripts/panjiva.js
@@ -9,48 +9,36 @@ $(document).ready(function() {
 });
 
 $(function() {
-//	$('html').mousemove(function(event){
-//		var timeCheck = event.timeStamp - lastMove;
-//		var btnPos = $('#btn').position();
-//		var distance = Math.min(Math.abs(btnPos.left-event.pageX), Math.abs(btnPos.top-event.pageY));
-//		var diff = Math.abs(distance - lastDistance) / Math.min(distance, lastDistance);
-//		//console.log(distance+" "+diff);
-//		if (distance < 25 || (distance < lastDistance && (diff > 0.25 || timeCheck > 200))) {
-//			var dir = (btnPos.left-event.pageX > 0) ? 1 : 0;
-//			mouseMoved(1, distance);
-//			lastMove = event.timeStamp;
-//			//console.log(" move ");
-//		}
-//		lastDistance = distance;
-//	});
 	$('html').mousemove(function(event) {
 		var btnPos = $('#btn').position();
 		var xDist = btnPos.left - event.pageX;
 		var yDist = btnPos.top - event.pageY;
-		var moveX = 1;
-		var moveY = 1;
+		var moveX = 10;
+		var moveY = 10;
 		var timeCheck = event.timeStamp - lastMove;
-		//if (xDist > 0) console.log((xDist > 0));
-		var newX = (xDist > 0) ? btnPos.left + moveX : btnPos.left - moveX;
-		var newY = (yDist > 0) ? btnPos.top + moveY : btnPos.top - moveY;
-		console.log(" xDist "+xDist+" xPrevDist "+xPrevDist+" yDist "+yDist+" yPrevDist "+yPrevDist);//+" "+btnPos.left+" "+btnPos.top);
+		//if (xDist > 0) console.log((xDist > 0)+" "+xDist);
+		//if (yDist > 0) console.log((yDist > 0)+" "+yDist);
+		var newX = (xDist > 0) ? btnPos.left + 1 : btnPos.left - moveX;
+		var newY = (yDist > 0) ? btnPos.top + 1 : btnPos.top - moveY;
+//		console.log(" xDist "+xDist+" btnPos.left "+btnPos.left+" moveX "+moveX+" newX "+newX);
+//		console.log(" xDist "+xDist+" xPrevDist "+xPrevDist+" yDist "+yDist+" yPrevDist "+yPrevDist);//+" "+btnPos.left+" "+btnPos.top);
 		if (((xDist > 0 && xPrevDist > xDist) && ((yDist >= 0 && yPrevDist >= yDist) || (yDist <= 0 && yPrevDist <= yDist)))
 			|| ((xDist < 0 && xPrevDist < xDist) && ((yDist >= 0 && yPrevDist >= yDist) || (yDist <= 0 && yPrevDist <= yDist))) 
-			|| ((yDist > 0 && yPrevDist > yDist) && (xDist >= 0 && xPrevDist >= xDist))
-			|| ((yDist < 0 && yPrevDist > yDist) && (xDist <= 0 && xPrevDist <= xDist))
+			|| ((yDist > 0 && yPrevDist > yDist) && ((xDist >= 0 && xPrevDist >= xDist) || (xDist <= 0 && xPrevDist <= xDist)))
+			|| ((yDist < 0 && yPrevDist < yDist) && ((xDist >= 0 && xPrevDist >= xDist) || (xDist <= 0 && xPrevDist <= xDist)))
 			) {
-//			xPrevDist = xDist;
-//			yPrevDist = yDist;
-			if (timeCheck > 50 ) {
+//			if (timeCheck > 250 ) {
 				lastMove = event.timeStamp;
-				//$('#btn').css({left: newX, top: newY});
-			}
+				console.log("before left: "+$('#btn').css("left"));
+				$('#btn').css({left: newX, top: newY});
+				console.log("after left: "+$('#btn').css("left"));
+//			}
 		}
 //		console.log(xDist+" "+yDist+" "+xPrevDist+" "+yPrevDist+" "+btnPos.left+" "+btnPos.top);
-		if (timeCheck > 25 ) {
+//		if (timeCheck > 25 ) {
 			xPrevDist = xDist;
 			yPrevDist = yDist;
-		}
+//		}
 		
 	});
 });
