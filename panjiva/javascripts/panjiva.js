96a9badd872ab94076f0fd1599da4192df122f22
frost <mfrost@bostontechnologies.com>
2012-01-10 01:28:41 -0800

    panjiva

--- panjiva/javascripts/panjiva.js
+++ panjiva/javascripts/panjiva.js
@@ -15,35 +15,51 @@ $(function() {
 		var btnPos = $('#btn').position(); 
 		var xDist = btnPos.left - event.pageX;
 		var yDist = btnPos.top - event.pageY;
-		var moveX = distanceModifier(xDist);
-		var moveY = distanceModifier(yDist);
-		var newX = (xDist > 0) ? btnPos.left + moveX : btnPos.left - moveX;
-		var newY = (yDist > 0) ? btnPos.top + moveY : btnPos.top - moveY;
-		if ( ((xDist > 0 && xPrevDist > xDist) && ((yDist >= 0 && yPrevDist >= yDist) || (yDist <= 0 && yPrevDist <= yDist)))
-			|| ((xDist < 0 && xPrevDist < xDist) && ((yDist >= 0 && yPrevDist >= yDist) || (yDist <= 0 && yPrevDist <= yDist))) ) {
-//			var timeCheck = event.timeStamp - lastMove;
-			if (newX < 50) newX = width;
-			if (newY < 50) newY = 0;
-			if (newX > width) newX = 0;
-			if (newY > height) newY = 0;
-			$('#btn').css({left: newX, top: newY});
+		if (event.timeStamp - lastMove > delay(Math.min(Math.abs(xDist), Math.abs(yDist)))) {
+			var moveX = distanceModifier(xDist);
+			var moveY = distanceModifier(yDist);
+			var newX = (xDist > 0) ? btnPos.left + moveX : btnPos.left - moveX;
+			var newY = (yDist > 0) ? btnPos.top + moveY : btnPos.top - moveY;
+			if (((xDist > 0 && xPrevDist > xDist) && ((yDist > 0 && yPrevDist > yDist) || (yDist < 0 && yPrevDist < yDist)))
+				|| ((xDist < 0 && xPrevDist < xDist) && ((yDist > 0 && yPrevDist > yDist) || (yDist < 0 && yPrevDist < yDist))) ) {
+				lastMove = event.timeStamp;
+				if (newX < 50) {
+					newX = width - (width/10);
+				}
+				if (newY < 50) {
+					newY = height - (height/10);
+				}
+				if (newX > width) {
+					newX = 0 + (width/10);
+				}
+				if (newY > height) {
+					newY = 0 + (height/10);
+				}
+				$('#btn').css({left: newX, top: newY});
+			}
+			xPrevDist = xDist;
+			yPrevDist = yDist;
 		}
-		xPrevDist = xDist;
-		yPrevDist = yDist;
 	});
 });
+
+function delay(distance) {
+	if (distance < 100) {
+		return 0;
+	} else {
+		return Math.min(distance, 1000);
+	}
+}
 
 function distanceModifier(distance) {
 	var px =  0;
 	if (distance < 25) {
-		px = 150;
-	} else if (distance >= 25 && distance < 50) {
 		px = 75;
-	} else if (distance >= 50 && distance < 125) {
+	} else if (distance < 125) {
 		px = 25;
-	} else if (distance >= 125 && distance < 250) {
+	} else if (distance < 250) {
 		px = 5;
-	} else {
+	} else if (distance < (width / 2)) {
 		px = 1;
 	}
 	return px;
