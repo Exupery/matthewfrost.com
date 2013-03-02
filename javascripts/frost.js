15720335d8bce307c4aa2f27433eb7c7d87ba147
Matthew Frost <mf@matthewfrost.com>
2013-03-02 15:47:16 -0500

    changed single quotes to double

--- javascripts/frost.js
+++ javascripts/frost.js
@@ -1,43 +1,43 @@
 $(function() {
-	$( '#oomframe' ).dialog({
+	$("#oomframe").dialog({
 			autoOpen: false,
 			draggable: false,
 			resizable: false,
-			show: 'clip',
-			hide: 'clip',
-			speed: 'fast',
+			show: "clip",
+			hide: "clip",
+			speed: "fast",
 			width: 790
 		});
-	$( '#wguframe' ).dialog({
+	$("#wguframe").dialog({
 			autoOpen: false,
 			draggable: false,
 			resizable: false,
-			show: 'clip',
-			hide: 'clip',
-			speed: 'fast',
+			show: "clip",
+			hide: "clip",
+			speed: "fast",
 			width: 790
 		});
-	$( '#cppframe' ).dialog({
+	$("#cppframe").dialog({
 			autoOpen: false,
 			draggable: false,
 			resizable: false,
-			show: 'clip',
-			hide: 'clip',
-			speed: 'fast',
+			show: "clip",
+			hide: "clip",
+			speed: "fast",
 			width: 790
 		});
-	$( '#maincontent' ).accordion({
+	$("#maincontent").accordion({
 		autoHeight: false,
 		navigation: true,
 		collapsible: false,
-		animated: 'bounceslide'
+		animated: "bounceslide"
 	});	
 	
-	$( '#headercontent' ).click(function(){
-		$( '#maincontent' ).accordion( 'activate' , 0 );
+	$("#headercontent").click(function(){
+		$("#maincontent").accordion("activate" , 0);
 	});
 	
-	$( ".projtitle" ).mouseover(function(){
+	$(".projtitle").mouseover(function(){
 		displayFull(this.id);
 	});
 });
@@ -48,14 +48,14 @@ $(document).ready(function() {
 });
 
 function showDialog(id) {
-	$( '#'+id ).dialog('open');
+	$("#"+id ).dialog("open");
 }
 
 function displayFull(id) {
-	var idhtml = $( '#'+id+'txtfull' ).html();
-	$( '#projectdetails' ).html(idhtml);
-	$( 'h2.projtitle' ).removeClass('whiteonblue').addClass('blueonwhite');
-	$( '#'+id ).addClass('whiteonblue');
+	var idhtml = $("#"+id+"txtfull").html();
+	$("#projectdetails").html(idhtml);
+	$("h2.projtitle").removeClass("whiteonblue").addClass("blueonwhite");
+	$("#"+id ).addClass("whiteonblue");
 	$("#"+id+"screen").click(function(){
 		showDialog(id+"frame");
 	});
@@ -65,37 +65,25 @@ function drawSkills(parent) {
 	var skills = ["Java","Ruby","JavaScript","PHP","Lua","C++","C#","Perl","MySQL","MongoDB","JSP","jQuery","GWT","AWS","Rails","Apex","CSS","Tomcat"];
 	var exp = [/*"Java"*/5,/*"Ruby"*/3,/*"JavaScript"*/4,/*"PHP"*/4,/*"Lua"*/2,/*"C++"*/2,/*"C#"*/2,/*"Perl"*/2,/*"MySQL"*/4,/*"MongoDB"*/3,/*"JSP"*/1,/*"jQuery"*/4,/*"GWT"*/2,/*"AWS"*/1,/*"Rails"*/3,/*"Apex"*/2,/*"CSS"*/4,/*"Tomcat"*/1];
 
-	var html = '';
+	var html = "";
 	for (var i=0; i<skills.length; i++) {
 		html += drawSkill(skills[i], exp[i]);
 	}
 
-	$('#'+parent).html(html);
+	$("#"+parent).html(html);
 }
 
 function drawSkill(skill, exp) {
-	var html = 	'<div class=\'skill\'>'+
-			    '<span class=\'skill_name\'>'+skill+'</span>'+
-				'<div class=\'skills_exp\'>';
+	var html = 	"<div class=\"skill\">"+
+			    "<span class=\"skill_name\">"+skill+"</span>"+
+				"<div class=\"skills_exp\">";
 	for (var i=0; i<5; i++) {
 		if (exp > i) {
-			html += '<div class=\'box filled\'></div>'
+			html += "<div class=\"box filled\"></div>"
 		} else {
-			html += '<div class=\'box\'></div>'
+			html += "<div class=\"box\"></div>"
 		}
 	}
-	html += '</div></div>';
+	html += "</div></div>";
 	return html;						
-}
-
-function oom() {
-	$( '#oomframe' ).dialog('open');
-}
-
-function wgu() {
-	$( '#wguframe' ).dialog('open');
-}
-
-function cpp() {
-	$( '#cppframe' ).dialog('open');
 }
