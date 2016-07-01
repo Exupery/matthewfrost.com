255fb2f60e7c94e1eb2db4142fd06c32098bd6ca
Matthew Frost <mf@matthewfrost.com>
2016-07-01 18:13:44 -0400

    fixed whitespace

--- javascripts/frost.js
+++ javascripts/frost.js
@@ -1,44 +1,44 @@
 $(function() {
-	$(".screenshotframe").dialog({
-			autoOpen: false,
-			draggable: false,
-			resizable: false,
-			show: "blind",
-			hide: "blind",
-			speed: "fast",
-			closeOnEscape: true,
-			width: "auto",
-			height: "auto"
-		});
+  $(".screenshotframe").dialog({
+    autoOpen: false,
+    draggable: false,
+    resizable: false,
+    show: "blind",
+    hide: "blind",
+    speed: "fast",
+    closeOnEscape: true,
+    width: "auto",
+    height: "auto"
+  });
 
-	$(".screenshot").click(function(event) {
-		event.stopPropagation();
+  $(".screenshot").click(function(event) {
+    event.stopPropagation();
     showDialog(this.id+"-frame");
   });
 
-	$(".screenshotframe").click(function(event) {
+  $(".screenshotframe").click(function(event) {
     hideDialog(this.id);
   });
 
-	$("#main, #projects").tabs({
-		heightStyle: "content",
-		animate: "easeOutCubic"
-	});
+  $("#main, #projects").tabs({
+    heightStyle: "content",
+    animate: "easeOutCubic"
+  });
 
-	$(".button").on("click", function() {
-  	ga("send", "event", "button", this.id);
-	});
+  $(".button").on("click", function() {
+    ga("send", "event", "button", this.id);
+  });
 
 });
 
 function showDialog(id) {
-	$("#"+id).dialog("open");
-	$("body").click(function() {
+  $("#"+id).dialog("open");
+  $("body").click(function() {
     hideDialog(id);
   });
-	ga("send", "event", "dialog", id);
+  ga("send", "event", "dialog", id);
 }
 
 function hideDialog(id) {
-	$("#"+id).dialog("close");
+  $("#"+id).dialog("close");
 }
